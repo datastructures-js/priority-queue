f6566479607fe0cf62346a6a45d369de44d79d27
Sean Derrow <sean.derrow@gmail.com>
2024-12-22 15:49:46 -0600

    Capture null returns in type definitions

--- src/minPriorityQueue.d.ts
+++ src/minPriorityQueue.d.ts
@@ -5,12 +5,12 @@ export class MinPriorityQueue<T> {
   [Symbol.iterator](): Iterator<T, any, undefined>;
   size(): number;
   isEmpty(): boolean;
-  front(): T;
-  back(): T;
+  front(): T | null;
+  back(): T | null;
   enqueue(value: T): MinPriorityQueue<T>;
   push(value: T): MinPriorityQueue<T>;
-  dequeue(): T;
-  pop(): T;
+  dequeue(): T | null;
+  pop(): T | null;
   remove(cb: (value: T) => boolean): T[];
   contains(cb: (value: T) => boolean): boolean;
   toArray(): T[];
--- src/priorityQueue.d.ts
+++ src/priorityQueue.d.ts
@@ -5,12 +5,12 @@ export class PriorityQueue<T> {
   [Symbol.iterator](): Iterator<T, any, undefined>;
   size(): number;
   isEmpty(): boolean;
-  front(): T;
-  back(): T;
+  front(): T | null;
+  back(): T | null;
   enqueue(value: T): PriorityQueue<T>;
   push(value: T): PriorityQueue<T>;
-  dequeue(): T;
-  pop(): T;
+  dequeue(): T | null;
+  pop(): T | null;
   remove(cb: (value: T) => boolean): T[];
   contains(cb: (value: T) => boolean): boolean;
   toArray(): T[];
